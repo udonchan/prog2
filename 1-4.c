54568f543640de6e0c6477062ea984100207896f
udonchan <udonchan@udonchan.org>
2010-10-03 22:41:32 +0900

    1-4

--- 1-4.c
+++ 1-4.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
-typedef struct gift{char code[10];char name[40];int price;}G;int main(){
-    G g[100];FILE *fp;if((fp = fopen( "gifts.dat", "r" ))!=NULL)for (int i=0;(fscanf(fp, "%s %s %d", g[i].code, g[i].name, &g[i].price)!=EOF) && i<100;i++)printf("code : %s\nname : %s\nprice : %d\n", i[g].code, (g+i)->name, g[i].price);}
+
+int main(){
+    typedef struct gift{char code[10];char name[40];int price;}G;
+    G g[100];
+    FILE *fp;
+    if((fp=fopen("gifts.dat","r"))!=NULL)
+	for(int i=0;(fscanf(fp, "%s %s %d", g[i].code, g[i].name, &g[i].price)!=EOF)&&i<100;i++)
+	    printf("code : %s\nname : %s\nprice : %d\n", i[g].code, (g+i)->name, g[i].price);
+}
