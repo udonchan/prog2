0c7edc2a8749d4c8a2b43bd5c189c768871d79ef
udonchan <udonchan@udonchan.org>
2010-10-03 22:39:45 +0900

    1-3

--- 1-3.c
+++ 1-3.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 main() {
-    typedef struct gift{char code[10];char name[40];int price;}h;
-    h g = { "JZK-30", "Jizake tsumeawase", 4500 };
-    printf("code : %s\nname : %s\nprice : %d\n", g.code, g.name, g.price);
+    typedef struct gift{char code[10];char name[40];int price;}G;
+    G g={"JZK-30","Jizake tsumeawase",4500};
+    printf("code : %s\nname : %s\nprice : %d\n",g.code,g.name,g.price);
 }
