432eb5a6f0ae46ba8e18c826c36ead9ba929f26b
udonchan <udonchan@udonchan.org>
2010-10-03 21:50:36 +0900

    3-1

--- 3-1.c
+++ 3-1.c
@@ -1,32 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int main (void) {
-    int max_length;
-    char c, *str, *p, buf[100];
-
-    printf("文字列最大長を入力してください :");
-    fgets(buf, 100, stdin);
-    sscanf(buf, "%d", &max_length);
-
-    if (max_length < 1)
-	return 0;
-
-    if ((str = malloc((size_t)(sizeof(char) * max_length))) == NULL ) {
-	fprintf(stderr, "malloc failed.\n");
-	return 1;
+main(){
+    int m,*s,*p, buf[8];
+    printf("文字列最大長を入力");
+    fgets(buf,8,stdin);
+    sscanf(buf,"%d",&m);
+    if(m>0){
+	if((s=malloc(m))!=NULL){
+	    printf("文字列を入力 ");
+	    p = s;
+	    for(int c;(c=getchar())!='\n';*s++ = c);
+	    while(p!=s--)putchar(toupper(*s));
+	    puts("");
+	}
     }
-
-    printf("文字列を入力してください : ");
-
-    p = str;
-    while ((c = getchar()) != '\n')
-	*str++ = c;
-    while(*p != *str--)
-	putchar(toupper(*str));
-    puts("");
-
-    free(p);
-    return 0;
 }
