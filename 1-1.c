a60affd8fd82af0bf17d19424d59c6437ec4089c
udonchan <udonchan@udonchan.org>
2010-10-03 22:37:20 +0900

    1-1

--- 1-1.c
+++ 1-1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 main(){
     FILE *fp;
-    char id[8], name[20];
-    int i;
-    if((fp = fopen( "gifts.dat", "r" ))!=NULL)while(fscanf( fp, "%s\t%s\t%d\n", id, name, &i)!=EOF)printf("%s %s %d\n", id, name, i);
+    int id[8],name[20],i;
+    if((fp=fopen("gifts.dat","r"))!=NULL)
+	while(fscanf(fp,"%s\t%s\t%d\n",id,name,&i)!=EOF)
+	    printf("%s %s %d\n", id, name, i);
 }
