772f6ec22cdbfab6adbe847fe0b37027d8d43b71
udonchan <udonchan@udonchan.org>
2010-10-03 21:18:45 +0900

    fuga

--- 1-5.c
+++ 1-5.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
-#define INPUT_FILE "gifts.dat"
 
-struct gift {
-    char code[10];
-    char name[40];
-    int price;
-};
-
-
-char *stu (char *s) {
-    char *p;
-    for (p = s; *p; p++) *p = toupper(*p);
-    return (s);
+typedef struct gift {char code[10];char name[40];int price;}G;
+char *u (char *s) {
+    for (char *p = s; *p; p++) *p = toupper(*p)
+        ;return s;
 }
 
-int main() {
-    int i, parm;
-    struct gift g[100];
-    FILE *fp;
-
-    if( (fp = fopen( INPUT_FILE, "r" )) == NULL ) {	
-        fprintf(stderr, "cannot open inputfile.\n");
-	return 1;
+main() {
+    G g[100];FILE *fp;
+    if((fp = fopen("gifts.dat","r" ))!= NULL) {
+	for(int i,parm;EOF!=(parm=fscanf(fp, "%s %s %d", g[i].code, g[i].name, &g[i].price));i++){
+	    printf("code : %s\nname : %s\nprice : ", g[i].code, u((i+g)->name));
+	    if(parm >= 3)printf("%d\n", g[i].price);else puts("xxx");
+	}
     }
-
-    for (i = 0; (parm = fscanf(fp, "%s %s %d", g[i].code, g[i].name, &g[i].price)) != EOF; i++ ) {
-	printf("code : %s\nname : %s\nprice : ", g[i].code, stu(g[i].name));
-	if(parm >= 3)
-	    printf("%d\n", g[i].price);
-	else
-	    puts("xxx");
-    }
-
-    return 0;
 }
