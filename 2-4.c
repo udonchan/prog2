3e3ad37a832a7f3b1653be82657528f4c63cc13a
udonchan <udonchan@udonchan.org>
2010-10-05 08:35:13 +0900

    golf

--- 2-4.c
+++ 2-4.c
@@ -1,8 +1,1 @@
-c;main(t){
-    for(char s[99],*p;gets(s);puts(&s))
-	for(p=s;*p;p++,c++)
-	    if(toupper(*p)==84&&*(p+1)=='h'){
-		*p=*(p+1)=43;t++;
-	    }
-    printf("strings:%d\nth:%d\n",c,t);
-}
+c;main(t){for(char*p,s[99];gets(s);puts(&s))for(p=s;*p;p++,c++)if(toupper(*p)==84&&*(p+1)=='h')*p=*(p+1)=43,t++;printf("strings:%d\nth:%d\n",c,t);}
