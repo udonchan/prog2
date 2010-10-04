6085aa8b2642a6abbf66e03a8c4ada41afdee5bf
udonchan <udonchan@udonchan.org>
2010-10-04 21:01:31 +0900

    golf

--- 3-2.c
+++ 3-2.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-main(){
-    int i,*n[5];
-    puts("名前を最大5 人分入力して下さい．");
-    for(i=0;i<5&&(fgets(n[i]=malloc(20),100,stdin)!=NULL);i++);
-    for(i=0;*(n+i+1)!=NULL;i++)printf("%x %s",i[n],i[n]);
+*n[5];
+main(i){
+    puts("名前を最大5人分入力して下さい．");
+    for(i=0;i<5&&gets(n[i++]=malloc(20)););
+    for(i=0;*(n+i);i++)printf("%x %s\n",i[n],i[n]);
 }
