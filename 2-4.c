356c3b3070684976d771865004cf76b9cdcf9c1f
udonchan <udonchan@udonchan.org>
2010-10-04 20:00:12 +0900

    golf

--- 2-4.c
+++ 2-4.c
@@ -1,24 +1,8 @@
-#include <stdio.h>
-
-int main( void ) {
-    int char_count = 0, th_count = 0;
-    char str[100];
-    char *p;
-
-    while (fgets(str, 100, stdin) != NULL) {
-        for (p = str; *p != '\0'; p++) {
-            char_count++;
-            if (strncmp(p, "th", 2) == 0 || strncmp(p, "Th", 2) == 0) {
-                th_count++;
-                *p = '+';
-                *++p = '+';
-            }
-        }
-        fputs(str, stdout);
-    }
-
-    printf("文字数：%d\n", char_count);
-    printf("thの出現回数：%d\n", th_count);
-
-    return 0;
+c;main(t){
+    for(char s[99],*p;gets(s);puts(&s))
+	for(p=s;*p;p++,c++)
+	    if(toupper(*p)==84&&*(p+1)=='h'){
+		*p=*(p+1)=43;t++;
+	    }
+    printf("strings:%d\nth:%d\n",c,t);
 }
