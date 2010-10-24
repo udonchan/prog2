cf55b30387ac3fd41019e7403c35abf7000f419b
udonchan <udonchan@udonchan.org>
2010-10-24 22:31:16 +0900

    hoge

--- 1-5.c
+++ 1-5.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-typedef struct gift {char code[10];char name[40];int price;}G;
-char *u (char *s) {
-    for (char *p=s; *p=toupper(*p);p++);
+typedef struct{char code[10];char name[40];int price;}G;
+char*u(char*s){
+    for (char*p=s;*p=toupper(*p);p++);
     return s;
 }
-main() {
-    G g[100];FILE *fp;
-    if((fp = fopen("gifts.dat","r" ))!= NULL) {
-	for(int i,parm;EOF!=(parm=fscanf(fp, "%s %s %d", g[i].code, g[i].name, &g[i].price));i++){
-	    printf("code : %s\nname : %s\nprice : ", g[i].code, u((i+g)->name));
-	    if(parm >= 3)printf("%d\n", g[i].price);else puts("xxx");
+main(FILE*fp){
+    G g[100];
+    if(fp=fopen("gifts.dat","r")) 
+	for(int i,parm;EOF!=(parm=fscanf(fp,"%s%s%d",i[g].code,i[g].name,&i[g].price));i++){
+	    printf("code : %s\nname : %s\nprice : ",g[i].code,u(g[i].name));
+	    if(parm-2)printf("%d\n", g[i].price);
+	    else puts("xxx");
 	}
-    }
-}
+}	
